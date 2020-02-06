1782dcba01992aca81e169db2b93eea471dafbb8
yoongoo <dbsrlehs92@gmail.com>
2020-02-07 01:22:41 +0900

    no message

---
+++ 1934.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+
+int gcd(int a, int b) 
+{
+	if (b == 0) {
+		return a;
+	}
+	
+	return gcd(b, a%b);
+}
+
+int main()
+{
+	int T;
+	scanf("%d", &T);
+	
+	while (T--) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		int g = gcd(a, b);
+		long long ans = a * b / g;
+		printf("%d\n", ans);
+	}
+	
+	return 0;
+}
