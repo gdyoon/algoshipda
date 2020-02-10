073281701e18e710d2f6dee626b924beedc70a17
yoongoo <dbsrlehs92@gmail.com>
2020-02-10 22:40:12 +0900

    no message

---
+++ 10950.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main() {
+	int T;
+	int a, b;
+	
+	scanf("%d", &T);
+	
+	for(int i = 0; i < T; i++) {
+		scanf("%d %d", &a, &b);
+		printf("%d\n", a + b);
+	}
+	
+	return 0;
+}
