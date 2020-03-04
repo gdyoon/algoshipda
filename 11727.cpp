b5d57a55137f95341cc3e12a5a4dd64da1c0686c
yoongoo <dbsrlehs92@gmail.com>
2020-03-04 21:30:19 +0900

    no message

---
+++ 11727.cpp
@@ -0,0 +1,24 @@
+/**
+ * NOTE: https://www.acmicpc.net/problem/11727
+ */
+ #include <cstdio>
+ 
+ int d[1000];
+ 
+ int main() {
+ 	int N;
+ 	
+ 	scanf("%d", &N);
+ 	
+ 	d[0] = 1;
+ 	d[1] = 1;
+ 	
+ 	for (int i=2; i<=N; i++) {
+ 		d[i] = d[i-1] + d[i-2]*2 ;
+ 		d[i] %= 10007;
+	}
+	 
+	printf("%d\n", d[N]);
+ 	
+ 	return 0;
+ }
